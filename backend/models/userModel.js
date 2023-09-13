862ac4295f8cce8c618134a509e42306d2c8acdc
assrat007 <asratadane169@gmail.com>
2023-09-13 13:04:25 +0300

    modelcreated

---
+++ backend/models/userModel.js
@@ -0,0 +1,76 @@
+const mongoose = require('mongoose');
+const Schema= mongoose.Schema
+
+const userSchema = new Schema(
+
+    {
+        fullname:{
+            type: String,
+        },
+        age:{ type: Number, default:0},
+        email:{
+            type:String,
+            unique:true,
+            required:true,
+        },
+        password:{
+            type:String,   
+            required:true, 
+        },
+        avatar:{
+            type:String,
+        },
+        pictures:[String],
+        skintone:{type:String},
+        language:[String],
+        phone:{type:String},
+        info:{
+          region:{
+            type:String,
+          },
+          city:{
+            type:String,
+          },
+          nationality:{type:String},
+          accadamic:{type:String},
+          expriance:[String]
+        },
+        talent:{
+            type:[String],
+            enum:["Swimming ","Car Driving ","Motor"," Cycling","Cycling", " Heavy Car Driving","  Sket Boarding",
+             " Karate","  Tenise",  "Foot Ball","  Gym", " Runing ", " Modeling" ," Circus",  " Wood Working" ," Metal Working", 
+             " Vocal", " Drum" ," Lead Guitar", " Base Guitar ","Saxphone","  Trampet"," Violin","Cello" ," Fluet", " Keyboard" ,
+             " Piano"]
+        },
+        isActive:{type:Boolean,default:true},
+        isAdmin:{type:Boolean,default:false},
+        isEmailVarified:{type:Boolean,default:false}
+    }
+);
+userSchema.methods.toPtofileJSON = function(){
+    return{
+        id:this._id,
+        fullname:this.fullname,
+        age:this.age,
+        email:this.email,
+        password:this.password,
+        isEmailVarified:this.isEmailVarified,
+        avatar:this.avatar,
+        pictures:this.pictures,
+        skintone:this.skintone,
+        language:this.language,
+        phone:this.phone,
+
+
+    }
+}
+userSchema.methods.toAdmin = function(){
+    return{
+        id:this._id,
+        email:this.email,
+        password:this.password
+    }
+}
+
+userSchema.index({name:"text"})
+module.exports= mongoose.model("User",userSchema)
