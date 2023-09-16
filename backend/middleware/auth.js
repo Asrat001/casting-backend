cad9b65e1d0a5880f35191a51857737d80f74fb6
Aron Hunde <aronhunde20@gmail.com>
2023-09-16 23:34:13 +0300

    admin middleware done

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -18,7 +18,24 @@ const isAuthenticated = catchAsyncErrors(async(req,res,next) => {
     next();
 });
 
-module.exports={isAuthenticated};
 
+const isAdminMiddleware = async (req, res, next) => {
+  
+  const token = req.cookies["access_token"];
 
+  if(!token){
+      res.status(400).json({error:"Invalid token"});
+  }
 
+  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+  user = await User.findById(decoded.id);
+if(user.isAdmin==true)
+{
+next()
+}
+else {
+  res.status(403).json({ error: 'Unauthorized' }); // User is not an admin, return an error response
+}
+};
+  module.exports={isAdminMiddleware,isAuthenticated};
