a6e98ac2860f50c3ab3cd07bed659fb5b82409b1
assrat007 <asratadane169@gmail.com>
2023-11-30 10:48:19 +0300

    regeisterd

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -25,7 +25,7 @@ const registerUser = asyncHandler(async (req, res) => {
     const userExists = await User.findOne({ email });
 
     if (userExists) {
-      res.status(400).json({message:true});
+      res.status(400).json({message:'User already registered'});
       
     }
 
